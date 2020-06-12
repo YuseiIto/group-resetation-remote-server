edf9d7c72cda65f146b2aea015b09fe9f3bf32ea
Yusei Ito <yuseikun@Outlook.jp>
2020-06-13 02:21:56 +0900

    Send command from server!

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+"use strict";
 const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 50000 });
-console.log("WSS started!");
+
 wss.on("connection", (ws) => {
     console.log("Connection established with a client!");
+    let isReady = false;
+
+    setInterval(function() {
+        if (isReady) {
+            ws.send("next");
+        }
+    }, 1500);
     ws.on("message", (message) => {
         console.log(`received a massage : ${message}`);
-        ws.send("Hello");
+        if (message === "ready") {
+            isReady = true;
+            console.log("Now client is ready!");
+        }
     });
 });
